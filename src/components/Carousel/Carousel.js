8c47aa3505f5d9642cfcd6eae8bf2e4d4435b0a9
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-08 15:42:44 +0200

    Add initial files & Carousel component

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import data from '../../data.json'
+import './Carousel.scss';
+
+class Carousel extends React.Component {
+
+  render() {
+
+    return (
+      <section className="container">
+        <h1> Experiences in Paris</h1>
+
+      </section>
+    );
+  }
+}
+
+export default Carousel;
