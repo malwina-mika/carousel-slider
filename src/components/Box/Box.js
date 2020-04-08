63d4f8a9d2a22a8e9126cae83a6cf3b13414a9ff
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-08 16:54:24 +0200

    Add: Stars component & functionality to giving rating

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Stars from '../Stars/Stars'
 import './Box.scss'
 
 const Box = ({ id, title, body, rating, img, ratingAmount, review, isActive
@@ -7,6 +8,7 @@ const Box = ({ id, title, body, rating, img, ratingAmount, review, isActive
       <img src={img} alt={title} />
       <p>{body}</p>
       <h4>{title}</h4>
+      <Stars rating={rating} ratingAmount={ratingAmount} review={review} />
 
     </div>
   )
---
+++ src/components/Stars/Stars.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Stars.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+class Stars extends React.Component {
+  state = {
+    starsConfig: [
+      {
+        id: 1,
+        active: false,
+      },
+      {
+        id: 2,
+        active: false,
+      },
+      {
+        id: 3,
+        active: false,
+      },
+      {
+        id: 4,
+        active: false,
+      },
+      {
+        id: 5,
+        active: false,
+      },
+    ],
+    reviewState: false,
+  };
+
+  review(rating) {
+    this.setState(({ starsConfig }) => {
+      return starsConfig.map(i => {
+        if (i.id <= rating) {
+          i.active = 'active';
+        } else {
+          i.active = 'inactive';
+        }
+        return i;
+      });
+    });
+  }
+
+  giveReview(star) {
+    this.setState({ reviewState: star });
+  }
+
+  mouseOutOfStars() {
+    this.setState(({ starsConfig }) => {
+      return starsConfig.map(i => {
+        i.active = false;
+        return i;
+      });
+    });
+  }
+
+  render() {
+    const { rating, review } = this.props;
+    const { starsConfig, reviewState } = this.state;
+
+    return (
+      <div onMouseOut={() => this.mouseOutOfStars()}>
+        {starsConfig.map(i => (
+          <FontAwesomeIcon
+            key={i.id}
+            onClick={() => this.giveReview(i.id)}
+            onMouseOver={() => this.review(i.id)}
+            icon={
+              (i.active
+                ? i.active === 'active'
+                : i.id <= (reviewState || rating || review))
+                ? faStar
+                : farStar
+            }
+            className={
+              (i.active
+                ? i.active === 'active'
+                : i.id <= (reviewState || rating))
+                ? 'active'
+                : ''
+            }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+
+export default Stars;
